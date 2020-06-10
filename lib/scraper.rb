93cd2296fa986f45c3e45fde64a287b7dfa1db5d
Jonathan East <jonathan_east@ymail.com>
2020-06-10 00:59:34 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,15 +1,21 @@
+
 require 'open-uri'
 require 'pry'
 
 class Scraper
 
   def self.scrape_index_page(index_url)
-    
+    html = open('https://learn-co-curriculum.github.io/student-scraper-test-page/index.html')
+    doc = Nokogiri::HTML(html)
+    binding.pry
+
+    students = {}
+
+
   end
 
   def self.scrape_profile_page(profile_url)
-    
+
   end
 
 end
-
