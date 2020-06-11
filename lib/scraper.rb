97266f851555af92ef345fe241b9c1c4bea7e9d7
Jonathan East <jonathan_east@ymail.com>
2020-06-11 02:06:32 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -18,7 +18,6 @@ class Scraper
 
     #iterate through the students hash
     document.css("div.student-card").each do |student|
-      name = student.css("h4.student-name").text
       students = {
         :name => student.css("h4.student-name").text,
         :location => student.css("p.student-location").text,
@@ -29,8 +28,32 @@ class Scraper
     students_array
   end
 
-  def self.scrape_profile_page(profile_url)
+  # profile anchors: document.css("div.social-icon-container > a")
+  # profile quote: document.css("div.profile_quote").text
+  # bio: document.css("div.description-holder p").text
 
+
+  def self.scrape_profile_page(profile_url)
+    html = profile_url
+    document = Nokogiri::HTML(open(html))
+    student_profiles = {}
+    profile_social_links = document.css("div.social-icon-container > a").map { |profile_social_link|
+      profile_social_link.attribute("href").value
+    }.each do |social_link|
+      if social_link.include?("twitter")
+        student_profiles[:twitter] = social_link
+      elsif social_link.include?("linkedin")
+        student_profiles[:linkedin] = social_link
+      elsif social_link.include?("github")
+        student_profiles[:github] = social_link
+      else
+        student_profiles[:blog] = social_link
+      end
+    end
+    student_profiles[:profile_quote] = document.css("div.profile-quote").text
+    student_profiles[:bio] = document.css("div.description-holder p").text
+    student_profiles
+    # binding.pry
   end
 
 end
